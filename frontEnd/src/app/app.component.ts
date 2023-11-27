8d86f4c84143bcd3af097c6cce8433ed66d1f7e8
Louie Brown <78971075+LouieJBR@users.noreply.github.com>
2023-11-27 11:46:56 +0000

    adding inital component files and mock data

--- frontEnd/src/app/app.component.ts
+++ frontEnd/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ProductsComponent} from "./products/products.component";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontEnd';
+  title = 'COM661 Front End';
 }
