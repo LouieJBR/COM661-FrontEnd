8d824f0e06bcb450fe6f780865fd0117928214b1
Louie Brown <78971075+LouieJBR@users.noreply.github.com>
2023-11-28 13:21:08 +0000

    storignPaginationDetailsInStorage

--- frontEnd/src/app/products/allProducts/products.component.ts
+++ frontEnd/src/app/products/allProducts/products.component.ts
@@ -10,20 +10,25 @@ export class ProductsComponent{
   constructor(public webService: WebService ) {}
 
   ngOnInit(){
+    if (sessionStorage['page']){
+      this.page = Number(sessionStorage['page']);
+    }
     this.product_list = this.webService.getAllProducts(this.page)
   }
 
   previousPage(){
     if (this.page > 1) {
       this.page = this.page -1;
+      sessionStorage['page'] = this.page;
       this.product_list = this.product_list =
         this.webService.getAllProducts(this.page)
     }
   }
 
   nextPage(){
-      this.page = this.page  +1;
-      this.product_list = this.product_list =
+    this.page = this.page  +1;
+    sessionStorage['page'] = this.page;
+    this.product_list = this.product_list =
         this.webService.getAllProducts(this.page)
   }
   product_list: any = [];
