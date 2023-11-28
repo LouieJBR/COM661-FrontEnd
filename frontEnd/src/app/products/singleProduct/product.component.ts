2b08a29bb056dad1893bfb4e33cd3e9188a0489d
Louie Brown <78971075+LouieJBR@users.noreply.github.com>
2023-11-28 13:58:37 +0000

    addingBasicReviewForm

--- frontEnd/src/app/products/singleProduct/product.component.ts
+++ frontEnd/src/app/products/singleProduct/product.component.ts
@@ -1,6 +1,7 @@
 import {Component} from "@angular/core";
 import {WebService} from "../../web.service";
 import {ActivatedRoute} from "@angular/router";
+import {FormBuilder} from "@angular/forms";
 
 @Component({
   selector: 'product',
@@ -10,11 +11,23 @@ import {ActivatedRoute} from "@angular/router";
 export class ProductComponent {
   product_list: any;
   reviews: any = [];
+  reviewForm: any;
   constructor(private webService: WebService,
-              private route: ActivatedRoute) {}
+              private route: ActivatedRoute,
+              private formBuilder: FormBuilder) {}
 
   ngOnInit(){
+    this.reviewForm = this.formBuilder.group({
+      username: '',
+      comment: '',
+      stars: 5
+    })
+
     this.product_list = this.webService.getProductById(this.route.snapshot.params['id'])
     this.reviews = this.webService.getReviews(this.route.snapshot.params['id'])
+  }
+
+  onSubmit(){
+    console.log(this.reviewForm.value)
   }
 }
