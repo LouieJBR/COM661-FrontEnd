76c42bfab91bdc163fd10862d689da6b48e0ccd1
Louie Brown <78971075+LouieJBR@users.noreply.github.com>
2023-12-01 14:34:55 +0000

    newProductPage

--- frontEnd/src/app/components/products/products/products.component.ts
+++ frontEnd/src/app/components/products/products/products.component.ts
@@ -9,18 +9,54 @@ import {WebService} from "../../../web.service";
 export class ProductsComponent{
   constructor(public webService: WebService ) {}
 
+  products: any = [];
+
+
   ngOnInit(){
     if (sessionStorage['page']){
       this.page = Number(sessionStorage['page']);
     }
-    this.product_list = this.webService.getAllProductsPagination(this.page)
+
+    this.webService.getAllProductsPagination(this.page).subscribe((data: any) => {
+      this.products = data;
+      this.createCardGrid(); // Call function to create card grid after data retrieval
+    });  }
+
+  createCard(name: string, price: string, type: string, size: string, id:string) {
+    const col = document.createElement('div');
+    col.classList.add('col-md-4'); // Adjust column size based on your design (e.g., col-md-4, col-lg-3)
+    const card = document.createElement('div');
+    card.classList.add('card', 'm-3');
+    card.innerHTML = `
+      <div class="card-body">
+        <h5 class="card-title">${name}</h5>
+        <p class="card-text">Price: ${price}</p>
+        <p class="card-text">Type: ${type}</p>
+        <p class="card-text">Size: ${size}</p>
+      </div>
+    `;
+    card.addEventListener('click', () => {
+      window.location.href = "/products/" + id;
+    });      // Redirect to the URL associated with the product when the card is clicked
+
+    col.appendChild(card);
+    return col;
+  }
+
+  createCardGrid() {
+    let container = document.getElementById('cardContainer');
+    this.products.forEach((product: any) => {
+      const { name, price, type, size, _id } = product;
+      const colElement = this.createCard(name, price, type, size, _id);
+      container?.appendChild(colElement);
+    });
   }
 
   previousPage(){
     if (this.page > 1) {
       this.page = this.page -1;
       sessionStorage['page'] = this.page;
-      this.product_list = this.product_list =
+      this.products = this.products =
         this.webService.getAllProductsPagination(this.page)
     }
   }
@@ -28,9 +64,9 @@ export class ProductsComponent{
   nextPage(){
     this.page = this.page  +1;
     sessionStorage['page'] = this.page;
-    this.product_list = this.product_list =
-        this.webService.getAllProductsPagination(this.page)
+    this.products = this.products =
+      this.webService.getAllProductsPagination(this.page)
+
   }
-  product_list: any = [];
   page: number = 1;
 }
